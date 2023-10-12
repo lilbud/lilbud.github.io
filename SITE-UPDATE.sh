24bc79f411a96d094cf472c175687b8244199052
Brian <bvw20002010@gmail.com>
2023-10-11 20:06:56 -0400

    update

--- SITE-UPDATE.sh
+++ SITE-UPDATE.sh
@@ -1,4 +1,2 @@
 git fetch bootleg-covers main
 git subtree pull --prefix assets/img/graphics/bootleg-covers/covers bootleg-covers main --squash -m "add covers"
-git commit -am "new covers"
-git push
